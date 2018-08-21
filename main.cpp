8e12b26b88e2e40da543defa3842ac95345ad300
Michael Wang <michael-wenyuan.wang@cn.abb.com>
2018-08-21 16:13:00 +0800

    [Reason] Optimization: now we don't need to select the repo.dot and repo.env, and start selecting workspace from HOME instead of /home [Change] Add default root component and platform variable and use getenv to get the HOME directory

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 
 // TODO: Calulate the path according to the working dir environment
 
+static const char DEFAULT_ROOT_COMPONENT[] = "Esmeralda";
+static const char DEFAULT_PLATFORM[] = "armbuildroot";
 //#define MY_DEBUG
 
 QWaitCondition waitCondition;
@@ -17,10 +19,14 @@ QMutex complete;
 
 int main(int argc, char *argv[])
 {
+    QString rootComponent(DEFAULT_ROOT_COMPONENT);
+    QString platform(DEFAULT_PLATFORM);
+
     for (int i = 1; i < argc; ++i) {
         QString arg(argv[i]);
 //        qDebug() << arg;
 
+        // TODO: Add argument to support specifying root component and platform
         if (arg == "-V") {
             qDebug() << "Version: " + QString(version);
             return 0;
@@ -35,22 +41,33 @@ int main(int argc, char *argv[])
 
 #ifdef MY_DEBUG
     w.dot.workingDir = "/home/abb/mtws";
-    QString dotFile = "/home/abb/mtws/obj/armbuildroot/Esmeralda/repo.dot";
-    QString envFile = "/home/abb/mtws/obj/armbuildroot/Esmeralda/repo.env";
 #else
     w.dot.workingDir = QFileDialog::getExistingDirectory(0, "Please choose your working directory",
-                                                     "/home",
+                                                     getenv("HOME"),
                                                      QFileDialog::ShowDirsOnly
                                                      | QFileDialog::DontResolveSymlinks);
 
+#endif
     if (w.dot.workingDir.isEmpty()) {
         qDebug() << "Working directory is not specified!";
-        return(1);
+        return(-1);
     }
 
-    QString dotFile = QFileDialog::getOpenFileName(0, "Please choose repo.dot", w.dot.workingDir + "/obj");
-    QString envFile = QFileDialog::getOpenFileName(0, "Please choose repo.env", w.dot.workingDir + "/obj");
-#endif
+    QString dotFile = w.dot.workingDir + "/obj/" + platform + "/" + rootComponent + "/repo.dot";
+    QFile fileDot(dotFile);
+    if (!fileDot.exists()) {
+        qDebug() << dotFile + " doesn't exist!";
+        return(-1);
+    }
+    qDebug() << "dotFile is " << dotFile;
+
+    QString envFile = w.dot.workingDir + "/obj/" + platform + "/" + rootComponent + "/repo.env";
+    QFile fileEnv(envFile);
+    if (!fileEnv.exists()) {
+        qDebug() << envFile + " doesn't exist!";
+        return(-1);
+    }
+    qDebug() << "envFile is " << envFile;
 
     RepoEnv repoEnv(envFile);
 
